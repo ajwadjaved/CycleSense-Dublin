4f4247a10e7a8e85926e008617fcdee372f3103f
Ajwad <ajwadjaved@gmail.com>
2023-03-23 17:33:52 +0000

    finalized weatherAPI schema, setting up crontab now

---
+++ weather_connection/DBConnector.py
@@ -0,0 +1,75 @@
+import credentials
+import dbinfo
+from sqlalchemy import create_engine, text
+
+
+class DBConnector:
+    # establish connection with amazon aws rds;
+    # engine is meant to only be created once per application process
+    # so it should be a class variable that has only one instance
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+        credentials.DB_USER, credentials.DB_PW, dbinfo.DB_URI, dbinfo.DB_PORT,
+        dbinfo.DB_NAME),
+        echo=True)
+
+    def create_database(self):
+        # text() converts string to compatible sql command
+        create_database = text("CREATE DATABASE IF NOT EXISTS dublin_bikes_db")
+        # engine.begin() starts a transaction and auto-commits each sql command
+        # "with" block would close the connection at the end automatically
+        with self.engine.begin() as connection:
+            connection.execute(create_database)
+
+    def create_static_station_table(self):
+        create_static_station_table = text("""
+                CREATE TABLE IF NOT EXISTS weather (
+                    date DATE,
+                    time TIME,
+                    weather VARCHAR(128),
+                    degrees FLOAT,
+                    PRIMARY KEY (date,time)
+                );
+            """)
+        with self.engine.begin() as connection:
+            connection.execute(create_static_station_table)
+
+    # def create_dynamic_availability_table(self):
+    #     create_dynamic_availability_table = text("""
+    #             CREATE TABLE IF NOT EXISTS weather (
+    #                 date DATE,
+    #                 weather VARCHAR(128),
+    #                 degrees FLOAT,
+    #                 PRIMARY KEY (NUMBER)
+    #             );
+    #             """)
+    #     with self.engine.begin() as connection:
+    #         connection.execute(create_dynamic_availability_table)
+
+    def test_connection(self):
+        check_column_types = text("""
+                SELECT COLUMN_NAME, DATA_TYPE
+                FROM information_schema.COLUMNS
+                WHERE TABLE_NAME = 'weather'
+            """)
+        with self.engine.begin() as connection:
+            for column in connection.execute(check_column_types):
+                print(column)
+
+    def insert_static_data(self, weatherdata):
+        print(weatherdata)
+        with self.engine.begin() as connection:
+            insert_static = text("""
+                INSERT INTO weather(date, time, weather, degrees)
+                VALUES ('{}','{}','{}',{});
+                                """
+                                    .format(weatherdata[0],
+                                            weatherdata[1],
+                                            weatherdata[2],
+                                            weatherdata[3])
+                                    )
+            connection.execute(insert_static)
+
+    @staticmethod
+    def process_str(string):
+        # 2 single quotes: first single quote escapes the second in a sql insert query
+        return string.replace("'", "''")
---
+++ weather_connection/openweather.py
@@ -0,0 +1,51 @@
+import weatherapi
+import requests
+from datetime import datetime
+import os
+import requests
+from datetime import datetime
+from DBConnector import DBConnector
+
+db_connector = DBConnector()
+
+
+def query_weatherAPI():
+    output = []
+
+    URL = "https://api.openweathermap.org/data/2.5/weather"
+    today = datetime.today().strftime("%Y/%m/%d")
+    curr_time = datetime.now().strftime("%H/%M/%S")
+
+    params = {
+        "q": "Dublin,IE",
+        "appid": weatherapi.API,
+        "units": "metric"
+    }
+
+    # Make the API request
+    response = requests.get(URL, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Extract the weather data from the JSON response
+        data = response.json()
+        description = data['weather'][0]['description']
+        temp = data['main']['temp']
+    else:
+        print("Error:", response.status_code)
+
+    output.append(today)
+    output.append(curr_time)
+    output.append(description)
+    output.append(temp)
+
+    return output
+
+    # print(today)
+    # print(description)
+    # print(temp)
+
+# print(query_weatherAPI())
+# db_connector.create_database()
+# db_connector.create_static_station_table()
+db_connector.insert_static_data(query_weatherAPI())
